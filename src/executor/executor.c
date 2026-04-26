19eb6ffc0616ed627c08b5be6df9ac1b8e4898b9
Keremalp Demiroz <keremalpde@gmail.com>
2026-04-26 16:58:04 +0300

    [BUG FIX] open fds in childs fixed

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -3,12 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mahmmous <mahmmous@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kedemiro <kedemiro@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 15:25:00 by mahmmous          #+#    #+#             */
-/*   Updated: 2026/04/10 15:25:00 by mahmmous         ###   ########.fr       */
+/*   Updated: 2026/04/26 16:56:17 by kedemiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// #include "WCMS.h"
+
+// static void	exec_pipe_left(t_sh *sh, t_ast_node *node, int *fd)
+// {
+// 	close(fd[0]);
+// 	dup2(fd[1], STDOUT_FILENO);
+// 	close(fd[1]);
+// 	sh->in_pipe = 1;
+// 	execute_ast(sh, node->left);
+// 	exit(sh->exit_status);
+// }
+
+// static void	exec_pipe_right(t_sh *sh, t_ast_node *node, int *fd)
+// {
+// 	close(fd[1]);
+// 	dup2(fd[0], STDIN_FILENO);
+// 	close(fd[0]);
+// 	sh->in_pipe = 1;
+// 	execute_ast(sh, node->right);
+// 	exit(sh->exit_status);
+// }
+
+// static void	execute_pipe(t_sh *sh, t_ast_node *node)
+// {
+// 	int		fd[2];
+// 	pid_t	pid_left;
+// 	pid_t	pid_right;
+// 	int		status;
+
+// 	if (pipe(fd) < 0)
+// 		return ;
+// 	pid_left = fork();
+// 	if (pid_left == 0)
+// 		exec_pipe_left(sh, node, fd);
+// 	pid_right = fork();
+// 	if (pid_right == 0)
+// 		exec_pipe_right(sh, node, fd);
+// 	close(fd[0]);
+// 	close(fd[1]);
+// 	waitpid(pid_left, &status, 0);
+// 	waitpid(pid_right, &status, 0);
+// 	if (WIFEXITED(status))
+// 		sh->exit_status = WEXITSTATUS(status);
+// }
+
+// void	execute_ast(t_sh *sh, t_ast_node *node)
+// {
+// 	if (!node)
+// 		return ;
+// 	if (node->type == AST_CMD)
+// 		execute_cmd(sh, node->cmd);
+// 	else if (node->type == AST_PIPE)
+// 		execute_pipe(sh, node);
+// }
+
 
 #include "WCMS.h"
 
@@ -16,7 +72,7 @@ static void	exec_pipe_left(t_sh *sh, t_ast_node *node, int *fd)
 {
 	close(fd[0]);
 	dup2(fd[1], STDOUT_FILENO);
-	close(fd[1]);
+	close(fd[1]); // EKLENDİ: Orijinal ucu kapat!
 	sh->in_pipe = 1;
 	execute_ast(sh, node->left);
 	exit(sh->exit_status);
@@ -26,7 +82,7 @@ static void	exec_pipe_right(t_sh *sh, t_ast_node *node, int *fd)
 {
 	close(fd[1]);
 	dup2(fd[0], STDIN_FILENO);
-	close(fd[0]);
+	close(fd[0]); // EKLENDİ: Orijinal ucu kapat!
 	sh->in_pipe = 1;
 	execute_ast(sh, node->right);
 	exit(sh->exit_status);
--- src/executor/executor_cmd.c
+++ src/executor/executor_cmd.c
@@ -6,20 +6,133 @@
 /*   By: kedemiro <kedemiro@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 15:00:00 by mahmmous          #+#    #+#             */
-/*   Updated: 2026/04/15 17:40:14 by kedemiro         ###   ########.fr       */
+/*   Updated: 2026/04/26 16:56:45 by kedemiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// #include "WCMS.h"
+// #include <errno.h>
+
+// static void	close_fds(t_sh *sh)
+// {
+// 	dup2(sh->fds[0], STDIN_FILENO);
+// 	dup2(sh->fds[1], STDOUT_FILENO);
+// 	close(sh->fds[0]);
+// 	close(sh->fds[1]);
+// }
+
+// static int	open_redir(t_redir *redir)
+// {
+// 	int	fd;
+
+// 	fd = -1;
+// 	if (redir->type == TOKEN_REDIR_IN)
+// 		fd = open(redir->file, O_RDONLY);
+// 	else if (redir->type == TOKEN_REDIR_OUT)
+// 		fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 	else if (redir->type == TOKEN_REDIR_APPEND)
+// 		fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+// 	else if (redir->type == TOKEN_HEREDOC)
+// 		fd = redir->heredoc_fd;
+// 	return (fd);
+// }
+
+// int	apply_redirections(t_sh *sh, t_redir *redirs)
+// {
+// 	int	fd;
+// 	int	target;
+
+// 	(void)sh;
+// 	while (redirs)
+// 	{
+// 		fd = open_redir(redirs);
+// 		if (fd < 0)
+// 		{
+// 			ft_putstr_fd("minishell: ", 2);
+// 			ft_putstr_fd(redirs->file, 2);
+// 			ft_putstr_fd(": ", 2);
+// 			ft_putendl_fd(strerror(errno), 2);
+// 			return (-1);
+// 		}
+// 		if (redirs->type == TOKEN_REDIR_IN)
+// 			target = STDIN_FILENO;
+// 		else
+// 			target = STDOUT_FILENO;
+// 		dup2(fd, target);
+// 		close(fd);
+// 		redirs = redirs->next;
+// 	}
+// 	return (0);
+// }
+
+// static void	child_process(t_sh *sh, t_command *cmd, char *path)
+// {
+// 	char	**envp;
+
+// 	if (cmd->redirs && apply_redirections(sh, cmd->redirs) < 0)
+// 		exit(1);
+// 	if (!path)
+// 	{
+// 		ft_putstr_fd("minishell: ", 2);
+// 		ft_putstr_fd(cmd->args[0], 2);
+// 		ft_putendl_fd(": command not found", 2);
+// 		exit(127);
+// 	}
+// 	envp = env_to_envp(sh);
+// 	execve(path, cmd->args, envp);
+// 	ft_putstr_fd("minishell: ", 2);
+// 	ft_putstr_fd(cmd->args[0], 2);
+// 	ft_putendl_fd(": execution failed", 2);
+// 	exit(126);
+// }
+
+// static void	exec_external(t_sh *sh, t_command *cmd)
+// {
+// 	pid_t	pid;
+// 	int		status;
+// 	char	*path;
+
+// 	close_fds(sh);
+// 	path = resolve_path(sh, cmd->args[0]);
+// 	update_lastcmd_env(sh, cmd, path, 0);
+// 	pid = fork();
+// 	if (pid == 0)
+// 		child_process(sh, cmd, path);
+// 	waitpid(pid, &status, 0);
+// 	if (WIFEXITED(status))
+// 		sh->exit_status = WEXITSTATUS(status);
+// }
+
+
+// void	execute_cmd(t_sh *sh, t_command *cmd)
+// { 
+// 	if (!cmd)
+// 		return ;
+// 	handle_assignments(sh, cmd);
+// 	if (!cmd->args || !(cmd->args[0]))
+// 		return ;
+// 	sh->fds[0] = dup(STDIN_FILENO);
+// 	sh->fds[1] = dup(STDOUT_FILENO);
+// 	if (cmd->redirs && apply_redirections(sh, cmd->redirs) < 0)
+// 	{
+// 		sh->exit_status = 1;
+// 		close_fds(sh);
+// 		return ;
+// 	}
+// 	if (cmd->builtin != NOT_BUILTIN)
+// 	{
+// 		exec_builtin(sh, cmd);
+// 		close_fds(sh);
+// 		return ;
+// 	}
+// 	exec_external(sh, cmd);
+// }
+	
 
 #include "WCMS.h"
 #include <errno.h>
 
-static void	close_fds(t_sh *sh)
-{
-	dup2(sh->fds[0], STDIN_FILENO);
-	dup2(sh->fds[1], STDOUT_FILENO);
-	close(sh->fds[0]);
-	close(sh->fds[1]);
-}
+// executor_utils.c (veya başka bir dosya) içine:
 
 static int	open_redir(t_redir *redir)
 {
@@ -65,7 +178,18 @@ int	apply_redirections(t_sh *sh, t_redir *redirs)
 	return (0);
 }
 
-static void	child_process(t_sh *sh, t_command *cmd, char *path)
+void	close_fds(t_sh *sh, t_command *cmd)
+{
+	if (cmd->builtin != BUILTIN_EXIT)
+	{
+		dup2(sh->fds[0], STDIN_FILENO);
+		dup2(sh->fds[1], STDOUT_FILENO);
+		close(sh->fds[0]);
+		close(sh->fds[1]);
+	}
+}
+
+void	child_process(t_sh *sh, t_command *cmd, char *path)
 {
 	char	**envp;
 
@@ -82,49 +206,70 @@ static void	child_process(t_sh *sh, t_command *cmd, char *path)
 	execve(path, cmd->args, envp);
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(cmd->args[0], 2);
-	ft_putendl_fd(": execution failed", 2);
+	ft_putstr_fd(": execution failed\n", 2);
 	exit(126);
 }
 
-static void	exec_external(t_sh *sh, t_command *cmd)
+static void	exec_in_main(t_sh *sh, t_command *cmd)
 {
 	pid_t	pid;
 	int		status;
-	char	*path;
 
-	close_fds(sh);
-	path = resolve_path(sh, cmd->args[0]);
-	update_lastcmd_env(sh, cmd, path, 0);
-	pid = fork();
-	if (pid == 0)
-		child_process(sh, cmd, path);
-	waitpid(pid, &status, 0);
-	if (WIFEXITED(status))
-		sh->exit_status = WEXITSTATUS(status);
+	if (cmd->builtin != NOT_BUILTIN)
+	{
+		sh->fds[0] = dup(STDIN_FILENO);
+		sh->fds[1] = dup(STDOUT_FILENO);
+		if (cmd->redirs && apply_redirections(sh, cmd->redirs) < 0)
+		{
+			sh->exit_status = 1;
+			return (close_fds(sh, cmd));
+		}
+		exec_builtin(sh, cmd);
+		close_fds(sh, cmd);
+	}
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+			child_process(sh, cmd, resolve_path(sh, cmd->args[0]));
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			sh->exit_status = WEXITSTATUS(status);
+	}
 }
 
+static void	exec_in_pipe(t_sh *sh, t_command *cmd)
+{
+	if (cmd->builtin != NOT_BUILTIN)
+	{
+		if (cmd->redirs && (apply_redirections(sh, cmd->redirs) < 0))
+			exit(1);
+		exec_builtin(sh, cmd); // pwd çalıştı
+		
+		// BUILTIN BİTTİ: Çocuk süreç ölmeden önce açık kalan standart boruları (0 ve 1)
+        // manuel olarak kapatıyoruz ki Valgrind şikayet etmesin.
+        // NOT: Sadece STDIN ve STDOUT'u kapatıyoruz.
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
+		
+		exit(sh->exit_status); // Şimdi güvenle çıkabilirsin!
+	}
+	else
+	{
+		// External komutlarda close yapmaya gerek yok, execve her şeyi devralır.
+		child_process(sh, cmd, resolve_path(sh, cmd->args[0]));
+	}
+}
 
 void	execute_cmd(t_sh *sh, t_command *cmd)
-{ 
+{
 	if (!cmd)
 		return ;
 	handle_assignments(sh, cmd);
 	if (!cmd->args || !(cmd->args[0]))
 		return ;
-	sh->fds[0] = dup(STDIN_FILENO);
-	sh->fds[1] = dup(STDOUT_FILENO);
-	if (cmd->redirs && apply_redirections(sh, cmd->redirs) < 0)
-	{
-		sh->exit_status = 1;
-		close_fds(sh);
-		return ;
-	}
-	if (cmd->builtin != NOT_BUILTIN)
-	{
-		exec_builtin(sh, cmd);
-		close_fds(sh);
-		return ;
-	}
-	exec_external(sh, cmd);
+	if (sh->in_pipe == 0)
+		exec_in_main(sh, cmd);
+	else
+		exec_in_pipe(sh, cmd);
 }
-	
